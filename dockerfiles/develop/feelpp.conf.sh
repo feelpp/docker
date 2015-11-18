af35619e442bd3a269d977fd248a71e9104ad189
Alexandre Ancel <alexandre.ancel@cemosis.fr>
2015-11-18 10:47:12 +0100

    Update: - Added environment variables for better path handling - Updated path variables and build scripts

--- dockerfiles/develop/feelpp.conf.sh
+++ dockerfiles/develop/feelpp.conf.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # set feelpp home
-export SRC_DIR=${HOME}/src
-mkdir -p $SRC_DIR
+export FEELPP_SRC_DIR=${HOME}/src
+mkdir -p ${FEELPP_SRC_DIR}
 
-export FEELPP_HOME=${SRC_DIR}/build
-mkdir -p $FEELPP_HOME
+export FEELPP_BUILD_DIR=${HOME}/build
+mkdir -p ${FEELPP_BUILD_DIR}
+
+export FEELPP_HOME=/usr/local
+mkdir -p ${FEELPP_HOME}
+
+export PATH=${FEELPP_HOME}/bin:${PATH}
+export LD_LIBRARY_PATH=${FEELPP_HOME}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${FEELPP_HOME}/lib/pkgconfig:$PKG_CONFIG_PATH
+export PYTHONPATH=${FEELPP_HOME}/lib/python2.7/site-packages:$PYTHONPATH
+export MANPATH=${FEELPP_HOME}/share/man:$MANPATH
 
 # bash functions
 pull_feelpp ()
 {
   echo "Pulling feelpp..."
-  cd $SRC_DIR
+  cd ${FEELPP_SRC_DIR}
   if [ -d feelpp ]
   then
 	 cd feelpp
@@ -26,11 +35,11 @@ pull_feelpp ()
 build_feelpp()
 {
   echo "Building Feel++..."
-  if [ -d $SRC_DIR/feelpp ]
+  if [ -d ${FEELPP_SRC_DIR}/feelpp ]
   then
-        cd $FEELPP_HOME
-    	${SRC_DIR}/feelpp/configure -r --cmakeflags="-DFEELPP_ENABLE_VTK_INSITU=ON "
-        make install-feelpp-lib
+        cd ${FEELPP_BUILD_DIR}
+    	${FEELPP_SRC_DIR}/feelpp/configure -r --cmakeflags="-DFEELPP_ENABLE_VTK_INSITU=ON -DCMAKE_INSTALL_PREFIX=${FEELPP_HOME} "
+        sudo make -j 16 install-feelpp-lib
   else
 	   echo "Feel++ source cannot be found. Please run pull_feelpp first."
   fi
