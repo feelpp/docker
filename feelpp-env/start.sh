176fc4efb9f569e06a75d63e294918d9316855a9
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-07-18 11:48:27 +0200

    fix for mongrestore feelpp/feelpp#953

--- feelpp-env/start.sh
+++ feelpp-env/start.sh
@@ -2,7 +2,7 @@
 
 if [ -d /feel/crbdb ]; then
     sudo service mongodb start
-    if [ -f /feel/crbdb/mongodb ]; then
+    if [ -d /feel/crbdb/mongodb ]; then
         /usr/lib/juju/mongo3.2/bin/mongorestore /feel/crbdb/mongodb
     fi
 fi
