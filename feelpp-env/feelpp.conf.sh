9ad1b4dbff8d55a3db57b2618e2d370c9fb830b0
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-03-24 09:28:36 +0100

    up image

--- feelpp-env/feelpp.conf.sh
+++ feelpp-env/feelpp.conf.sh
@@ -196,6 +196,8 @@ install_feelpp_module()
         pull_feelpp "${BRANCH}"
         ((exit_status= $exit_status || $?))
     fi
+    echo "--- [install_feelpp_module] sources"
+    ls ${FEELPP_SRC_DIR}/feelpp
     exit_status=0
     build_feelpp_module "${1}" "${2}" "${3}" "${4:-${DEFAULT_NJOBS}}" "${5}" "${*:7}"
     ((exit_status=$exit_status || $?));
