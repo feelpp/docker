b86a28dfa571e2e56ce710fff43a9de28aa67049
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2023-01-03 00:57:14 +0100

    add support for wasm and Qt 6.4.1

---
+++ feelpp-env/qt6/6.4-aqt/install_wasm_qt6.sh
@@ -0,0 +1,29 @@
+export QT_VERSION=6.4.1
+export QT_PATH=/opt/Qt
+
+export SDK_PATH=/opt/emsdk
+# export SDK_VERSION=3.1.23
+# recommended
+export SDK_VERSION=3.1.14
+#export SDK_SHA256=a2609fd97580e4e332acbf49b6cc363714982f06cb6970d54c9789df8e91381c
+
+ADDITIONAL_PACKAGES="sudo git openssh-client ca-certificates curl python3 locales"
+
+export DEBIAN_FRONTEND=noninteractive
+export    DEBCONF_NONINTERACTIVE_SEEN=true 
+export   QT_PATH=${QT_PATH} 
+export     QT_WASM=${QT_PATH}/${QT_VERSION}/wasm_32 
+export    EMSDK=${SDK_PATH} 
+export     EMSDK_NODE=${SDK_PATH}/node/14.18.2_64bit/bin/node 
+export    PATH=$PATH:${QT_PATH}/Tools/CMake/bin:${QT_PATH}/Tools/Ninja:${QT_PATH}/${QT_VERSION}/wasm_32/bin:${SDK_PATH}/upstream/bin:${SDK_PATH}/upstream/emscripten:${SDK_PATH}/node/14.18.2_64bit/bin:${SDK_PATH}
+
+dirname=$(dirname "$0")
+
+# Install emscripten
+sh $dirname/get_emsdk.sh
+
+# Get Qt binaries with aqt
+sh $dirname/get_qt.sh
+
+# Install the required packages
+sh $dirname/install_packages.sh
