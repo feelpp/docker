aeb66af9fe76d2e379d44cd15c4a70fc47a30bde
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2023-04-12 13:55:50 +0200

    up #50
    @vincentchabannes do we need other Qt modules ?

--- feelpp-env/qt6/6.5-aqt/get_qt.sh
+++ feelpp-env/qt6/6.5-aqt/get_qt.sh
@@ -29,11 +29,11 @@ echo
 echo '--> Download & install the Qt library using aqt'
 echo
 
-aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" wasm_singlethread
+aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" wasm_singlethread  -m qtcharts qtquick3d qtshadertools qtdatavis3d
 aqt install-tool -O "$QT_PATH" linux desktop tools_cmake
 aqt install-tool -O "$QT_PATH" linux desktop tools_ninja
 # Host Qt needed for cross-compilation
-aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" gcc_64
+aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" gcc_64  -m qtcharts qtquick3d qtshadertools qtdatavis3d
 
 pip3 freeze | xargs pip3 uninstall -y
 
